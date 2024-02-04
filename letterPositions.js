a749e0171038c38a40c2643de762da903464a066
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-04 15:00:16 -0500

    Added findKeyByValue function

--- letterPositions.js
+++ letterPositions.js
@@ -13,4 +13,10 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
+// Test
 console.log(letterPositions('lighthouse in the house'));
+const result = letterPositions('hello');
+assertArraysEqual(result['h'], [0]);
+assertArraysEqual(result['e'], [1]);
+assertArraysEqual(result['l'], [2, 3]);
+assertArraysEqual(result['o'], [4]);
