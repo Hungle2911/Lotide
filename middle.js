e277157527e89f5cd512124e3d7e195aed4eb061
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-15 18:34:15 -0500

    Add middleTest.js

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-const eqArrays = (a, b) => {
-  if (a.length !== b.length) return false;
-  else {
-    for (var i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-const assertArraysEqual = (a, b) => {
-  if (eqArrays(a, b) === false) console.log(`Assertion Failed: ${a} !== ${b}`);
-  else console.log(`Assertion Passed: ${a} === ${b}`);
-};
 // Middle function
 const middle = (arr) => {
   let result = []
@@ -27,4 +12,4 @@ const middle = (arr) => {
     return result
   }
 }
-console.log(middle([1, 2, 3, 4, 5]))
+module.exports = middle
