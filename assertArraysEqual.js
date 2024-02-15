bd1bbfead0c3b7b73342f1c414fb9397c6cdb1a8
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-15 18:25:57 -0500

    Add assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-const eqArrays = (a, b) => {
-  if (a.length !== b.length) return false;
-  else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays')
 const assertArraysEqual = (a, b) => {
   if (eqArrays(a, b) === false) console.log(`Assertion Failed: ${a} !== ${b}`);
   else console.log(`Assertion Passed: ${a} === ${b}`);
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3], [1, 2, 2])
+module.exports = assertArraysEqual
