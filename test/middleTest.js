2443bbd834056c6df66e1b222207a0db62c22464
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-15 19:13:11 -0500

    Update headTest with Chai & Mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle')
 //Test 
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[2, 3, 4])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), 3)
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[2, 3])
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
